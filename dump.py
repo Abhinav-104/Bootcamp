7192f7fae80cb09f725f749ed3076f626acb5b5f
Abhinav Raghunath <abhinavraghunath04@gmail.com>
2021-06-08 11:35:44 +0530

    Program to add details in database to a csv file.

---
+++ dump.py
@@ -0,0 +1,24 @@
+import csv
+import sys
+import psycopg2
+
+def dump(fname):
+	dbconn = psycopg2.connect("dbname=superhero")
+	cursor = dbconn.cursor()
+	f = open(fname, "w")
+	writer = csv.writer(f)
+	cursor.execute('SELECT name,gender FROM heroes')
+
+	for name,gender in cursor.fetchall():
+		writer.writerow([name, gender])
+
+
+	dbconn.commit()
+	f.close()
+
+
+def main():
+	fname = sys.argv[1]
+	dump(fname)
+
+main()
